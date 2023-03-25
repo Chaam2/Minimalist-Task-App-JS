e65246428fce72f6806fa5084131a201e949dba3
cham2 <dearcham2@gmail.com>
2023-03-26 02:33:03 +0900

    change background img

--- JS/background.js
+++ JS/background.js
@@ -3,6 +3,7 @@ const images = ["0.jpeg","1.jpeg","2.jpeg","3.jpeg"]
 const chosenImg = images[Math.floor(Math.random()*images.length)]
 
 const imgTag = document.createElement('img')
-imgTag.src = `img/${chosenImg}`
+//imgTag.src = `img/${chosenImg}`
+imgTag.src = `https://source.unsplash.com/random/?cuteanimal`
 
 document.querySelector('#bg-img').appendChild(imgTag)
